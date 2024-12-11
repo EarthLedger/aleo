452dfb3260d69adb4711edc8af314e6833c85247
chris <chris.zhu@ott.ca>
2024-12-11 13:59:49 +0800

    update

--- zk-update.sh
+++ zk-update.sh
@@ -17,12 +17,12 @@ echo "This script will update ZKWORK prover in your ubuntu system, and auto conf
 
 # if no VER or ADDR quit
 if [ -z "$VER" ] || [ -z "$ADDR" ] ; then
-  echo "Usage: $0 <zkwork-version> <receive-address>"
-  echo "Example: $0 0.2.3 aleo1spkkxewxj2dl2lgdps9xr28093p5nxsvjv55g2unmqfu0hmwyuysmf4qp3"
+  echo "Usage: $0 <receive-address>"
+  echo "Example: $0 aleo1spkkxewxj2dl2lgdps9xr28093p5nxsvjv55g2unmqfu0hmwyuysmf4qp3"
   exit 1
 fi
 
-wget -O aleo_prover-v${VER}_full.tar.gz https://gh-proxy.com/https://ghp.ci/https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
+wget -O aleo_prover-v${VER}_full.tar.gz https://ghp.ci/https://ghp.ci/https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
 
 tar -xvf aleo_prover-v${VER}_full.tar.gz -C /opt
 
